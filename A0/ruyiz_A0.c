ee6797ee8aaf5f8bb199eb487287fbc887528030
John Zhang <tianlang0704@gmail.com>
2015-09-30 17:32:13 -0700

    Directory for A0

---
+++ A0/ruyiz_A0.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("My first name is Ruyi, and My last name is Zhang.\n");
+	printf("Learning more about processes, threads and memory management in operating system excites me the most.\n");
+	printf("I am a little bit warried about learning the history of operating systems, I am not very good at that.\n");
+	printf("\"yourself\" is a word made up with 8 letters, how interesting!:P\n");
+	return 0;
+}
